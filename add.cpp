ef331ba04c0e20f3a46f466f168f809d377b6510
Sanskar Sharma <sanskaarsharma3@gmail.com>
2023-03-05 12:35:15 +0530

    add & diff committed

--- add.cpp
+++ add.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() 
 {
-    int a=5,b=7;
+    int a=10,b=7;
     cout<<"Sum of"<<a<<" & "<<b<<" is"<<(a+b)<<endl;
     return 0;
 }
---
+++ diff.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int a=5,b=7;
+    cout<<"Difference of"<<a<<" & "<<b<<" is"<<(a-b)<<endl;
+    return 0;
+}
