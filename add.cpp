90811a36d8c8b7c3f099411e9662db6007b66d76
Sanskar Sharma <sankysaa20@gmail.com>
2023-03-05 22:29:31 +0530

    new commit 2

--- add.cpp
+++ add.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() 
 {
     int a=10,b=7;
-    cout<<"Sum of"<<a<<" & "<<b<<" is"<<(a+b)<<endl;
+    cout<<"Sum of"<<a<<" and "<<b<<" is"<<(a+b)<<endl;
     return 0;
 }
--- diff.cpp
+++ diff.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() 
 {
     int a=5,b=7;
-    cout<<"Difference of"<<a<<" & "<<b<<" is"<<(a-b)<<endl;
+    cout<<"Difference of"<<a<<" and "<<b<<" is"<<(a-b)<<endl;
     return 0;
 }
